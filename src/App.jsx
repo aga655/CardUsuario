d42b18845f941a4f24c1f75c3e3e5e25ec3b6725
MARIN-Zenin <marin.lucas009@gmail.com>
2025-02-20 07:38:48 -0300

    comentários APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+// Importa os estilos e componentes necessários para estilizar e complementar o site
 import "./App.css";
-import "./components/Botao/Seguir.jsx"
+import "./components/Botao/Seguir.jsx"; 
 
+// Importa os componentes que compõe a interface
 import Seguir from "./components/Botao/Seguir";
 import Titulo from "./components/Header/Header";
 import Meio from "./components/Meio/Meio";
 import Lado from "./components/Lado/Lado";
 import Footer from "./components/Footer/Footer";
+
+// Imagens usadas no site
 import Agatha from "./assets/Agatha.png";
 import Ana from "./assets/Ana.png";
 import Zayra from "./assets/Zayra.png";
@@ -20,51 +24,52 @@ import Logo from "./assets/logo.png";
 function App() {
   return (
     <>
+      {/* Cabeçalho com título e logo */}
       <div className="body">
         <Titulo desafio="DESAFIO 1 de PROPS - REACT" logo={Logo} />
 
         <div className="container">
           <div className="cards">
+
+            {/* Cartões de informações com foto e hobby */}
             <div className="alinhar">
-            <Meio nome="Agatha França" img={Agatha} hobby="Hobby: Ler e assistir live da Shopee"/>
-            <Seguir/>
+              <Meio nome="Agatha França" img={Agatha} hobby="Hobby: Ler e assistir live da Shopee" />
+              <Seguir />
             </div>
 
             <div className="alinhar">
-            <Meio nome="Ana Beatriz" img={Ana} hobby="Hobby: Fofocar as 3am" />
-            <Seguir/>
+              <Meio nome="Ana Beatriz" img={Ana} hobby="Hobby: Fofocar as 3am" />
+              <Seguir />
             </div>
 
             <div className="alinhar">
-            <Meio nome="Anúbis" img={Anubis} hobby="Hobby: Programar" />
-            <Seguir/>
+              <Meio nome="Anúbis" img={Anubis} hobby="Hobby: Programar" />
+              <Seguir />
             </div>
 
             <div className="alinhar">
-            <Meio nome="Juan Lopes" img={Juan} hobby="Hobby: Programar" />
-            <Seguir/>
+              <Meio nome="Juan Lopes" img={Juan} hobby="Hobby: Programar" />
+              <Seguir />
             </div>
 
             <div className="alinhar">
-            <Meio nome="Lucas Marin" img={Lucas} hobby="Hobby: Cozinhar" />
-            <Seguir/>
+              <Meio nome="Lucas Marin" img={Lucas} hobby="Hobby: Cozinhar" />
+              <Seguir />
             </div>
 
             <div className="alinhar">
-            <Meio nome="Zayra França" img={Zayra} hobby="Hobby: Dormir" />
-            <Seguir/>
+              <Meio nome="Zayra França" img={Zayra} hobby="Hobby: Dormir" />
+              <Seguir />
             </div>
           </div>
 
+          {/* Exibição dos valores do grupo */}
           <div className="valores">
-            <Lado
-              valor1="Companheirismo"
-              valor2="Confiança"
-              valor3="Resiliência"
-            />
+            <Lado valor1="Companheirismo" valor2="Confiança" valor3="Resiliência" />
           </div>
         </div>
 
+        {/* Rodapé com contatos */}
         <Footer
           texto1="@JuanMOLopes 🤳"
           insta={Instagram}
@@ -78,5 +83,3 @@ function App() {
 }
 
 export default App;
-
-//teste
